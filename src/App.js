5e69c44d7e98d64b97f0798e6b1ba9802a6e5849
Laxman Desai <lolopaw24@gmail.com>
2021-05-25 01:24:32 +0530

    🐛 FIX: Remove unused  lightTheme variable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Dashboard from './containers/Dashboard'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from './styles/global'
-import { lightTheme, darkTheme } from './styles/theme'
+import { darkTheme } from './styles/theme'
 
 const App = () => {
   return (
