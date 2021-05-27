44282fef59970f71ad8d6d3111cd7198aca27bae
Laxman Desai <lolopaw24@gmail.com>
2021-05-27 22:43:35 +0530

    📦 NEW: Add react router and improve file directory

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Error page not found.</h1>
+      <p>
+        Return <Link to="/">home</Link>.
+      </p>
+    </div>
+  )
+}
+
+export default NotFound
