3bee7c87082dc74df6853d3cdbbe449d13aba97d
Laxman Desai <lolopaw24@gmail.com>
2021-06-03 08:49:46 +0530

    👌 IMPROVE: Init django backend

--- backend/resobin/asgi.py
+++ backend/resobin/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_project project.
+ASGI config for resobin project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resobin.settings')
 
 application = get_asgi_application()
--- backend/resobin/urls.py
+++ backend/resobin/urls.py
@@ -1,4 +1,4 @@
-"""my_project URL Configuration
+"""resobin URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', TemplateView.as_view(template_name="social_app/index.html")),  # <--
 ]
