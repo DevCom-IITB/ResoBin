973e6fb4f3fc45fa7bb29b9cf32dffbc0ead8a28
Laxman Desai <lolopaw24@gmail.com>
2021-06-06 05:39:30 +0530

    📦 NEW: Add react loader (not working)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { Suspense, useContext } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Loadable from 'react-loadable'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles, DarkTheme, LightTheme } from 'styles'
 import { ThemeContext } from 'context/ThemeContext'
-import { AdminView } from 'pages'
 import { Loader } from 'hoc'
+
+const AdminView = Loadable({
+  loader: () => import('pages/AdminView'),
+  loading: Loader,
+})
 
 const App = () => {
   const { theme } = useContext(ThemeContext)
--- frontend/src/hoc/Loader.jsx
+++ frontend/src/hoc/Loader.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 3px;
+  z-index: 1035;
+  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.2);
+`
+
+const LoaderTrack = styled.div`
+  position: relative;
+  height: 3px;
+  display: block;
+  width: 100%;
+  overflow: hidden;
+`
+
+const LoaderFill = styled.div`
+  &:after,
+  &:before {
+    content: '';
+    background: $theme-color;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    will-change: left, right;
+  }
+
+  &:before {
+    animation: mbar 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
+  }
+
+  &:after {
+    animation: m_s 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) infinite;
+    animation-delay: 1.15s;
+  }
+`
 
 const Loader = () => {
   return (
-    <div className="loader-bg">
-      <div className="loader-track">
-        <div className="loader-fill" />
-      </div>
-    </div>
+    <Container>
+      <LoaderTrack>
+        <LoaderFill />
+      </LoaderTrack>
+    </Container>
   )
 }
 
