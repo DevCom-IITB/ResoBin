57ecce156fb2df94fa7c89612173ad1e684a9fce
Laxman Desai <lolopaw24@gmail.com>
2021-06-11 18:49:01 +0530

    👌 IMPROVE: Add login, logout and check auth status views

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SignupView, GetCSRFToken
+from .views import SignupView, GetCSRFToken, LoginView, LogoutView, CheckAuthenticatedView
 
 urlpatterns = [
     path('signup', SignupView.as_view()),
+    path('authenticated', CheckAuthenticatedView.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
     path('csrf_token', GetCSRFToken.as_view()),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib import auth
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 from rest_framework.permissions import AllowAny
@@ -15,6 +16,14 @@ class GetCSRFToken(APIView):
         return Response({'success': 'CSRF cookie set'})
 
 
+class CheckAuthenticatedView(APIView):
+    def get(self, request, format=None):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Response({'isAuthenticated': 'error'}, status=401)
+        return Response({'isAuthenticated': 'success'})
+
+
 @method_decorator(csrf_protect, name='dispatch')
 class SignupView(APIView):
     permission_classes = (AllowAny, )
@@ -26,12 +35,12 @@ class SignupView(APIView):
         passwordAgain = data['passwordAgain']
         try:
             # signup input checks
-            if not User.objects.filter(username=username).exists():
-                return Response({'error': 'Username already exists'})
+            if User.objects.filter(username=username).exists():
+                return Response({'error': 'Username already exists'}, status=409)
             if password != passwordAgain:
-                return Response({'error': 'Passwords do not match'})
+                return Response({'error': 'Passwords do not match'}, status=422)
             if len(password) < 6:
-                return Response({'error': 'Password too short'})
+                return Response({'error': 'Password too short'}, status=422)
 
             user = User.objects.create_user(
                 username=username, password=password)
@@ -39,4 +48,33 @@ class SignupView(APIView):
             UserProfile.objects.create(user=user, full_name='', ldap='')
             return Response({'success': 'User account created successfully'})
         except:
-            return Response({'error': 'Something went wrong'})
+            return Response({'error': 'Something went wrong while signing up'})
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(APIView):
+    permission_classes = (AllowAny, )
+
+    def post(self, request, format=None):
+        data = self.request.data
+        username = data['username']
+        password = data['password']
+        try:
+            user = auth.authenticate(username=username, password=password)
+            if user is None:
+                return Response({'error': 'Username and password do not match'}, status=401)
+
+            auth.login(request, user)
+            return Response({'success': 'Logged in successfully'})
+        except:
+            return Response({'error': 'Something went wrong while logging in'})
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        try:
+            auth.logout(request)
+            return Response({'success': 'Successfully logged out'})
+        except:
+            return Response({'error': 'Something went wrong while logging out'})
