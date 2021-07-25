75b4c8fab937f412ccfc16284a03d07f86caea1b
Laxman Desai <lolopaw24@gmail.com>
2021-07-25 15:52:22 +0530

    🐛 FIX: Create local styles for antd Skeleton

--- src/components/courses/course-finder/item/CourseItem.jsx
+++ src/components/courses/course-finder/item/CourseItem.jsx
@@ -5,6 +5,7 @@ import {
   CourseItemMain,
   CourseItemSub,
 } from 'components/courses/course-finder/item'
+import { HEX2RGBA } from 'helpers'
 import { device } from 'styles/responsive'
 
 const Container = styled.li`
@@ -29,4 +30,17 @@ export const CourseItem = ({ ...props }) => {
   )
 }
 
-export const CourseItemLoading = ({ active }) => active && <Skeleton active />
+export const CourseItemLoading = ({ active }) =>
+  active && <StyledSkeleton active />
+
+const StyledSkeleton = styled(Skeleton)`
+  width: 100%;
+  margin: 1rem 0;
+
+  .ant-skeleton-content {
+    padding: 1.5rem 1rem;
+    border-radius: 0.5rem;
+    background: ${({ theme }) => HEX2RGBA(theme.darksecondary, 80)};
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+  }
+`
