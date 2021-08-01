24b276c7c3f1441dbed020943caad7242ff64fbe
Laxman Desai <lolopaw24@gmail.com>
2021-08-01 13:13:40 +0530

    👌 IMPROVE: Improved timetable v2

--- src/components/TimeTable/v2/TimeTable.jsx
+++ src/components/TimeTable/v2/TimeTable.jsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components/macro'
+
 import './TimeTable.css'
 
 const TrackHeader = ({ id, title }) => {
@@ -23,26 +25,85 @@ const RowHeader = ({ id, title }) => {
   )
 }
 
+const LectureItem = ({ track, rows, children }) => {
+  return (
+    <div
+      className="session session-1 track-1"
+      style={{ gridColumn: 'track-1', gridRow: 'time-0830 / time-0930' }}
+    >
+      {children}
+    </div>
+  )
+}
+
+const tracks = [
+  { id: '1', title: 'Monday' },
+  { id: '2', title: 'Tuesday' },
+  { id: '3', title: 'Wednesday' },
+  { id: '4', title: 'Thursday' },
+  { id: '5', title: 'Friday' },
+]
+
+const rows = [
+  { id: '0830', title: '08:30 am' },
+  { id: '0900', title: '09:00 am' },
+  { id: '0930', title: '09:30 am' },
+  { id: '1000', title: '10:00 am' },
+  { id: '1030', title: '10:30 am' },
+  { id: '1100', title: '11:00 am' },
+  { id: '1130', title: '11:30 am' },
+  { id: '1200', title: '12:00 pm' },
+  { id: '1230', title: '12:30 pm' },
+  { id: '1400', title: '14:00 pm' },
+  { id: '1430', title: '14:30 pm' },
+  { id: '1500', title: '15:00 pm' },
+  { id: '1530', title: '15:30 pm' },
+  { id: '1600', title: '16:00 pm' },
+  { id: '1630', title: '16:30 pm' },
+  { id: '1700', title: '17:00 pm' },
+  { id: '1730', title: '17:30 pm' },
+  { id: '1800', title: '18:00 pm' },
+  { id: '1830', title: '18:30 pm' },
+  { id: '1900', title: '19:00 pm' },
+  { id: '1930', title: '19:30 pm' },
+  { id: '2000', title: '20:00 pm' },
+  { id: '2030', title: '20:30 pm' },
+]
+
 const TimeTable = () => {
   return (
-    <div className="schedule">
-      <TrackHeader id="1" title="Monday" />
-      <TrackHeader id="2" title="Tuesday" />
-      <TrackHeader id="3" title="Wednesday" />
-      <TrackHeader id="4" title="Thursday" />
-      <TrackHeader id="5" title="Friday" />
+    <Container>
+      {tracks.map(({ id, title }, index) => (
+        <TrackHeader id={id} title={title} />
+      ))}
 
-      <RowHeader id="0830" title="8:30am" />
-      <RowHeader id="0900" title="9:00am" />
-      <RowHeader id="0930" title="9:30am" />
-      <RowHeader id="1000" title="10:00am" />
-      <RowHeader id="1030" title="10:30am" />
-      <RowHeader id="1100" title="11:00am" />
-      <RowHeader id="1130" title="11:30am" />
-      <RowHeader id="1200" title="12:00am" />
-      <RowHeader id="1230" title="12:30am" />
+      {rows.map(({ id, title }, index) => (
+        <RowHeader id={id} title={title} />
+      ))}
+
+      {rows.map((time, i) => {
+        const track = 1
+        return (
+          <div
+            className={`session track-${track}`}
+            style={{
+              gridColumn: 'track-1',
+              gridRow: `time-${rows[i]} / time-${rows[i + 1]}`,
+            }}
+            key={time}
+          />
+        )
+      })}
 
       <div
+        className="session track-1"
+        style={{ gridColumn: 'track-1', gridRow: 'time-0830 / time-0930' }}
+      >
+        <h3 className="title">Talk Title</h3>
+        <span className="time">8:30 - 9:30</span>
+      </div>
+
+      {/* <div
         className="session session-1 track-1"
         style={{ gridColumn: 'track-1', gridRow: 'time-0830 / time-0930' }}
       >
@@ -100,7 +161,8 @@ const TimeTable = () => {
         <span className="time">8:30 - 10:00</span>
         <span className="track">Track: 1</span>
         <span className="author">Mike Rofone</span>
-      </div>
+      </div> */}
+
       {/* <h2 class="time-slot" style="grid-column: times; grid-row: time-0900;">
 				9:00am
 			</h2>
@@ -191,8 +253,63 @@ const TimeTable = () => {
 			>
 				<h3 class="title">Lunch!</h3>
 			</div> */}
-    </div>
+    </Container>
   )
 }
 
 export default TimeTable
+
+/* Gridlines will need to be "named" in 24hr time
+  Switch '1fr' to 'auto' if height of slot shouldnt be proportional to lecture length */
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows:
+    [tracks] auto
+    [time-0830] 1fr
+    [time-0900] 1fr
+    [time-0930] 1fr
+    [time-1000] 1fr
+    [time-1030] 1fr
+    [time-1100] 1fr
+    [time-1130] 1fr
+    [time-1200] 1fr
+    [time-1230] 1fr
+    /* lunch break */
+    [time-1400] 1fr
+    [time-1430] 1fr
+    [time-1500] 1fr
+    [time-1530] 1fr
+    [time-1600] 1fr
+    [time-1630] 1fr
+    [time-1700] 1fr
+    /* evening break */
+    [time-1730] 1fr
+    [time-1800] 1fr
+    [time-1830] 1fr
+    [time-1900] 1fr
+    [time-1930] 1fr
+    [time-2000] 1fr
+    [time-2030] 1fr;
+  grid-template-columns:
+    [times] 3rem
+    [track-1-start] 1fr
+    [track-1-end track-2-start] 1fr
+    [track-2-end track-3-start] 1fr
+    [track-3-end track-4-start] 1fr
+    [track-4-end track-5-start] 1fr
+    [track-5-end];
+  grid-gap: 0.25rem;
+  background: #d7d7d7;
+
+  &::after {
+    content: '';
+    position: sticky;
+    top: 3rem;
+    z-index: 999;
+    display: block;
+    grid-row: tracks;
+    grid-column: track-1 / -1;
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+`
