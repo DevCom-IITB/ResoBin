931c5b19ad89c65625b39b36879f5ce331ef7bf1
Laxman Desai <lolopaw24@gmail.com>
2021-08-01 16:08:09 +0530

    👌 IMPROVE: Improve timetable lecture item styling

--- src/components/TimeTable/TimeTableLectureItem.jsx
+++ src/components/TimeTable/TimeTableLectureItem.jsx
@@ -1,89 +1,71 @@
 import styled from 'styled-components/macro'
 
-import { HEX2RGBA } from 'helpers'
+const TimeTableLectureItem = ({ title, track, row }) => {
+  // const style = {
+  //   gridColumn: `track-${track.id}-start`,
+  //   gridRow: `time-${row.start.id} / time-${row.end.id}`,
+  //   background: 'white',
+  // }
 
-const TimeTableLectureItem = ({ title, subtitle, img }) => {
   return (
-    <Container>
-      <h4>{title}</h4>
-      <AuthorWrapper>
-        <picture>
-          <img src={img} alt="" />
-        </picture>
-        <figcaption>{subtitle}</figcaption>
-      </AuthorWrapper>
-    </Container>
+    <GridItem row={row} track={track}>
+      <Container id={track.id}>
+        <h3>{title}</h3>
+        <span>
+          {row.start.title} - {row.end.title}
+        </span>
+      </Container>
+    </GridItem>
   )
 }
 
 export default TimeTableLectureItem
 
+const colors = (num) => {
+  switch (num) {
+    case '1':
+      return 'linear-gradient(to right, #00b09b, #96c93d)'
+    case '2':
+      return 'linear-gradient(to right, #000428, #004e92)'
+    case '3':
+      return 'linear-gradient(to right, #cb356b, #bd3f32)'
+    case '4':
+      return 'linear-gradient(to right, #f2994a, #f2c94c)'
+    case '5':
+      return 'linear-gradient(to right, #36d1dc, #5b86e5)'
+    case '6':
+      return 'linear-gradient(to right, #834d9b, #d04ed6)'
+    case '7':
+      return 'linear-gradient(to right, #fffc00, #fffc00)'
+    default:
+      return 'linear-gradient(to right, #666666, #aaaaaa)'
+  }
+}
+
+const GridItem = styled.div`
+  grid-row: time-${({ row }) => row.start.id} / time-${({ row }) => row.end.id};
+  grid-column: track-${({ track }) => track.id}-start;
+  background: white;
+`
+
 const Container = styled.div`
-  padding: 1rem;
-  margin-bottom: 1.25rem;
-  border: 1px solid hsl(0, 0%, 20%);
+  padding: 0.25rem 0.5rem;
   border-radius: 0.5rem;
-  font-size: 0.875rem;
-  font-family: system-ui, sans-serif;
-  color: hsl(0, 0%, 50%);
-  background: ${({ theme }) => HEX2RGBA(theme.darksecondary, 80)};
+  color: ${({ theme }) => theme.textColor};
+  background: ${({ id }) => colors(id)};
   cursor: pointer;
-  transition: all 0.2s ease-out;
+  transition: all 200ms ease-out;
+
+  & > h3 {
+    font-size: 1rem;
+  }
+
+  & > span {
+    display: block;
+    font-size: 0.75rem;
+  }
 
   &:hover {
-    background: ${({ theme }) => HEX2RGBA(theme.darksecondary, 90)};
-    transform: scale(1.005);
-  }
-
-  & > h4 {
-    margin-right: 3rem;
-    font-size: 0.875rem;
-    color: #ffffff;
-  }
-`
-
-const AuthorWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 0.625rem;
-
-  & > picture {
-    display: inline-block;
-    overflow: hidden;
-    margin-right: 0.625rem;
-    border: 1px solid hsl(0, 0%, 20%);
-    border-radius: 50%;
-    block-size: 1.625rem;
-    inline-size: 1.625rem;
-
-    & > img {
-      object-fit: cover;
-      block-size: 100%;
-      inline-size: 100%;
-    }
-  }
-`
-
-const backgroundColor = (theme) => styled`
-  .green {
-    background: linear-gradient(to right, #00b09b, #96c93d)
-  }
-  .orange {
-    background: linear-gradient(to right, #f2994a, #f2c94c)
-  }
-  .red {
-    background: linear-gradient(to right, #cb356b, #bd3f32)
-  }
-  .yellow {
-    background: linear-gradient(to right, #fffc00, #fffc00)
-  }
-  .blue {
-    background: linear-gradient(to right, #36d1dc, #5b86e5)
-  }
-  .pink {
-    background: linear-gradient(to right, #834d9b, #d04ed6)
-  }
-  .black {
-    background: linear-gradient(to right, #000428, #004e92)
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.4);
   }
 `
