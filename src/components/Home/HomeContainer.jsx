6e8f813c81e10b4f283ccd6e00b0f2f23c95b6ee
Laxman Desai <lolopaw24@gmail.com>
2021-08-21 03:46:17 +0530

    👌 IMPROVE: Add homepage layout

---
+++ src/components/Home/HomeContainer.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/macro'
+
+import HomeItem from 'components/Home/HomeItem'
+
+const HomeContainer = () => {
+  return (
+    <Container>
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+    </Container>
+  )
+}
+
+export default HomeContainer
+
+const Container = styled.li`
+  width: 100%;
+  padding: 1rem;
+`
