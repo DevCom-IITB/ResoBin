7b6a4cac979d6c50153667fe28605b7fc534c725
Laxman Desai <lolopaw24@gmail.com>
2021-08-21 03:46:50 +0530

    👌 IMPROVE: Add styled antd tabs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
+import { Helmet } from 'react-helmet-async'
+import styled from 'styled-components/macro'
+
+import HomeContainer from 'components/Home/HomeContainer'
+import { device } from 'styles/responsive'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home sweet home</h1>
-    </div>
+    <Container>
+      <Helmet>
+        <title>Courses - ResoBin</title>
+        <meta name="description" content="Courses availabe at IIT Bombay" />
+      </Helmet>
+
+      <HomeContainer />
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.div`
+  display: flex;
+  min-height: calc(100vh - ${({ theme }) => theme.headerHeight});
+
+  @media ${device.min.md} {
+    margin-left: ${({ theme }) => theme.asideWidthLeft};
+  }
+`
