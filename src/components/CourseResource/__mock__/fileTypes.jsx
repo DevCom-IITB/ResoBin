03b017026492986582a1294cd48513eef4dc5d24
Laxman Desai <lolopaw24@gmail.com>
2021-08-28 06:00:00 +0530

    📦 NEW: Add acceptible file format object

---
+++ src/components/CourseResource/__mock__/fileTypes.jsx
@@ -0,0 +1,45 @@
+// ? acceptable file types
+const fileTypes = [
+  {
+    type: 'application/pdf',
+    extention: 'pdf',
+    icon: 'https://image.flaticon.com/icons/svg/179/179483.svg',
+  },
+  {
+    type: 'application/msword',
+    extention: 'doc',
+    icon: 'https://image.flaticon.com/icons/svg/281/281760.svg',
+  },
+  {
+    type: 'application/rtf',
+    extention: 'rtf',
+    icon: 'https://image.flaticon.com/icons/svg/136/136539.svg',
+  },
+  {
+    type: 'image/png',
+    extention: 'png',
+    icon: 'https://image.flaticon.com/icons/svg/136/136523.svg',
+  },
+  {
+    type: 'image/jpeg',
+    extention: 'jpg',
+    icon: 'https://image.flaticon.com/icons/svg/136/136524.svg',
+  },
+  {
+    type: 'image/jpeg',
+    extention: 'jpeg',
+    icon: 'https://image.flaticon.com/icons/svg/136/136524.svg',
+  },
+  {
+    type: 'text/plain',
+    extention: 'txt',
+    icon: 'https://image.flaticon.com/icons/svg/136/136538.svg',
+  },
+  {
+    type: '*',
+    extention: '*',
+    icon: 'https://image.flaticon.com/icons/svg/136/136549.svg',
+  },
+]
+
+export default fileTypes
