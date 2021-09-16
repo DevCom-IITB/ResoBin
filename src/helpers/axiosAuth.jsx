793d82379f28170176595eabb66fb18e319531b9
Laxman Desai <lolopaw24@gmail.com>
2021-09-16 23:50:07 +0530

    👌 IMPROVE: Improve api auth

--- src/helpers/axiosAuth.jsx
+++ src/helpers/axiosAuth.jsx
@@ -1,24 +1,37 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
-import { toastError, toastSuccess } from 'components/toast'
+// import { toastError, toastSuccess } from 'components/toast'
 
-// ? Add csrf cookie to header
-const axiosAuth = axios.create({
-  xsrfHeaderName: 'X-CSRFToken',
+// // ? Add csrf cookie to header
+// const axiosAuth = axios.create({
+//   baseURL: 'http://localhost:8000',
+//   xsrfHeaderName: 'X-CSRFToken',
+//   xsrfCookieName: 'csrftoken',
+//   withCredentials: true,
+// })
+
+// // ? Display toasts for server's responses
+// axiosAuth.interceptors.response.use(
+//   (response) => {
+//     toastSuccess(response.data.success)
+//     return response
+//   },
+//   (error) => {
+//     toastError(`Error occurred, try again later.`)
+//     return Promise.reject(error)
+//   }
+// )
+
+export const APIInstance = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  header: {
+    Authorization: `Bearer ${Cookies.get('token')}`,
+  },
   xsrfCookieName: 'csrftoken',
-  withCredentials: true,
+  xsrfHeaderName: 'X-CSRFToken',
+  timeout: 30000,
+  // withCredentials: true,
 })
 
-// ? Display toasts for server's responses
-axiosAuth.interceptors.response.use(
-  (response) => {
-    toastSuccess(response.data.success)
-    return response
-  },
-  (error) => {
-    toastError(`Error occurred, try again later.`)
-    return Promise.reject(error)
-  }
-)
-
-export default axiosAuth
+export default APIInstance
