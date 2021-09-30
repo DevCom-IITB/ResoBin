97fb6a1b5714eb6af9289896bcbb204ad9cd9954
Laxman Desai <lolopaw24@gmail.com>
2021-09-30 12:39:25 +0530

    📦 NEW: Add review template

---
+++ src/components/CourseReview/reviewTemplate.jsx
@@ -0,0 +1,32 @@
+export default `
+<h1 id="course-review">Course review</h1>
+<br />
+
+<h2 id="details">Details</h2>
+<ul>
+  <li>Year and semester taken: </li>
+  <li>Professor: </li>
+  <li>Attendance criteria: </li>
+  <li>Time spent on course (h/week):</li>
+  <li>Overall rating (on 10): </li>
+</ul>
+<br />
+
+<h2 id="prerequisites-necessary-if-any-">Prerequisites necessary (if any)</h2>
+<br />
+<br />
+
+<h2 id="course-difficulty">Course difficulty</h2>
+<p>[i.e. how difficult the tests/assignments were and how strict the grading was?]</p>
+<br />
+<br />
+
+<h2 id="overview">Overview</h2>
+<p>[i.e. the expected learnings and future aspects]</p>
+<br />
+<br />
+
+<h2 id="personal-opinion">Personal opinion</h2>
+<br />
+<br />
+`
