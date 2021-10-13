502a6d2a0954dbd13098c1da80e71f466a3a7d7e
Laxman Desai <lolopaw24@gmail.com>
2021-10-13 13:16:27 +0530

    🐛 FIX: Rename and merge old files

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,2 +1,2 @@
-export { default as ContributorList } from './ContributorList'
-export { default as ContactForm } from './ContactForm'
+export { default as DeveloperList } from './DeveloperList'
+export { default as ContactContainer } from './ContactContainer'
