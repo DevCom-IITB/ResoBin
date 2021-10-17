47767db91ea7e2a8e38e0769ec10d820abe5c2f0
Laxman Desai <lolopaw24@gmail.com>
2021-10-17 14:48:56 +0530

    🐛 FIX: Update course favorite page to use api instead of cache

--- src/components/Favourites/FavouritesContainer.jsx
+++ src/components/Favourites/FavouritesContainer.jsx
@@ -1,90 +1,58 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import debounce from 'lodash/debounce'
-import { useState, useEffect, useCallback } from 'react'
-import { useSelector } from 'react-redux'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components/macro'
 
+import { API } from 'api'
 import { CourseList, CourseSearch } from 'components/CourseFinder'
+import { Aside } from 'components/shared'
 import { toastError } from 'components/toast'
-import { searchAsync } from 'helpers'
-import { selectFavouriteCourses } from 'store/userSlice'
-
-const searchFields = ['Code', 'Title', 'Description']
+import { useQueryString } from 'hooks'
 
 const FavouritesContainer = () => {
-  const location = useLocation()
-  const history = useHistory()
-  const queryString = new URLSearchParams(location.search)
+  const { getQueryString } = useQueryString()
 
-  const [search, setSearch] = useState(queryString.get('q') || '')
+  const [loading, setLoading] = useState(true)
+  const [courseData, setCourseData] = useState([])
 
-  const setQueryString = (query) => {
-    // No change
-    if (query === (queryString.get('q') || '')) return
-
-    // update query string or clear query string if query is empty
-    if (query) queryString.set('q', query)
-    else queryString.delete('q')
-
-    // reset pagination
-    queryString.delete('p')
-
-    history.push({
-      pathname: location.pathname,
-      search: `?${queryString.toString()}`,
-    })
+  const fetchCourses = async (params) => {
+    try {
+      setLoading(true)
+      const response = await API.profile.favorites({ params })
+      setCourseData(response)
+    } catch (error) {
+      toastError(error)
+    } finally {
+      setLoading(false)
+    }
   }
-
-  const [, setCourseDataFiltered] = useState([])
-
-  const favouriteCourses = useSelector(selectFavouriteCourses)
-
-  // ? search input state
-  const handleChange = (event) => setSearch(event.currentTarget.value)
-
-  // ? loading status while searching
-  const [loadingResults, setLoadingSearchResults] = useState(true)
-
-  // ? searching courses asynchronously
-  const searchCourses = (searchParams) => {
-    searchAsync(searchParams)
-      .then(setCourseDataFiltered)
-      .then(() => setQueryString(searchParams.keywords))
-      .then(() => setLoadingSearchResults(false))
-      .catch((error) => {
-        toastError(error.message)
-        setLoadingSearchResults(false)
-      })
-  }
-
-  // ? debounce search input change to provide smoother search experience
-  const searchCoursesDebounced = useCallback(debounce(searchCourses, 400), [])
 
   useEffect(() => {
-    setLoadingSearchResults(true)
-    searchCoursesDebounced({
-      dataSrc: favouriteCourses,
-      dataKeys: searchFields,
-      keywords: search,
-    })
-  }, [favouriteCourses, search, searchCoursesDebounced])
+    const filter = getQueryString()
+    const params = {
+      q: filter.q,
+      search_fields: 'code,title,description',
+      page: filter.p,
+    }
+
+    fetchCourses(params)
+  }, [getQueryString])
 
   return (
     <Container>
       <CourseSearch
-        loading={loadingResults}
-        value={search}
-        onChange={handleChange}
+        loading={loading}
+        setLoading={setLoading}
         showFilter={false}
-        filterState={null}
       />
 
       <CourseList
         title="Favourites"
-        courseCodes={favouriteCourses}
-        loading={loadingResults}
+        count={courseData.count}
+        courseList={courseData.results}
+        loading={loading}
+        setLoading={setLoading}
       />
+
+      <Aside title="My friends" />
     </Container>
   )
 }
