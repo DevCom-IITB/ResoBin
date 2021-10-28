b523f3ad03d701be50b7bf3d1a489f231e696407
Laxman Desai <lolopaw24@gmail.com>
2021-10-29 04:25:52 +0530

    👌 IMPROVE: Clean courseSlice and fix tags

--- src/store/courseSlice.jsx
+++ src/store/courseSlice.jsx
@@ -1,15 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
 
 import { API } from 'api'
 
 // ? async actions
-export const getCourseSlots = createAsyncThunk(
-  'courses/getCourseSlots',
-  async () =>
-    axios.get('https://run.mocky.io/v3/108c5a12-32e4-424a-bbc3-98f7ecd983d8')
-)
-
 export const getDepartmentList = createAsyncThunk(
   'courses/getDepartmentList',
   API.departments.list
@@ -33,38 +26,13 @@ const courseSlice = createSlice({
   name: 'course',
 
   initialState: {
-    list: [],
     listMinified: [],
-    slots: [],
     loading: false,
-    checksum: '',
-    lastUpdated: '',
     departments: [],
     semesters: [],
   },
 
-  reducers: {
-    updateChecksum: (state, { payload }) => {
-      state.checksum = payload.checksum
-      state.lastUpdated = payload.lastUpdated
-    },
-  },
-
   extraReducers: {
-    [getCourseSlots.fulfilled]: (state, { payload }) => {
-      state.slots = payload.data.map((course) => ({
-        ...course,
-        Code: course.Code.replaceAll(' ', ''),
-      }))
-      state.loading = false
-    },
-    [getCourseSlots.pending]: (state) => {
-      state.loading = true
-    },
-    [getCourseSlots.rejected]: (state) => {
-      state.loading = false
-    },
-
     [getDepartmentList.fulfilled]: (state, { payload }) => {
       state.departments = payload
       state.loading = false
@@ -100,13 +68,10 @@ const courseSlice = createSlice({
   },
 })
 
-// ? actions
-export const { updateChecksum } = courseSlice.actions
-
 // ? selectors
 export const selectCourseAPILoading = (state) => state.course.loading
+export const selectCourseListMinified = (state) => state.course.listMinified
 export const selectDepartments = (state) => state.course.departments
 export const selectSemesters = (state) => state.course.semesters
-export const selectCourseListMinified = (state) => state.course.listMinified
 
 export default courseSlice.reducer
