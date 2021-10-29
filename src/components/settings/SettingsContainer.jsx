8fbf0cdd97a762d846977d3a97cdcafc8eb3777b
Laxman Desai <lolopaw24@gmail.com>
2021-10-29 10:21:32 +0530

    👌 IMPROVE: Add settings container

---
+++ src/components/settings/SettingsContainer.jsx
@@ -0,0 +1,24 @@
+import { DarkmodeToggle } from 'components/settings'
+
+const SettingsContainer = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Switch theme</h1>
+      <DarkmodeToggle />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eveniet
+        voluptates asperiores modi expedita architecto quod optio numquam
+        aperiam sapiente nemo omnis soluta a amet sunt ad quia error aut
+        tenetur. Consequuntur quos perferendis nesciunt est odio fugiat autem
+        aliquam iste iusto voluptatum, itaque quam sint dolorum possimus atque
+        reiciendis et cumque consequatur? Nihil consectetur placeat vero! Hic
+        iste nesciunt libero rerum dolor minus magni tempore dolore accusantium
+        tenetur iusto repudiandae, fugiat, repellendus debitis enim, reiciendis
+        voluptas adipisci?
+      </h1>{' '}
+    </div>
+  )
+}
+
+export default SettingsContainer
--- src/components/settings/index.jsx
+++ src/components/settings/index.jsx
@@ -1,2 +1,2 @@
-/* eslint-disable import/prefer-default-export */
 export { default as DarkmodeToggle } from './DarkmodeToggle'
+export { default as SettingsContainer } from './SettingsContainer'
