bae97e80265ba888d03e1d15aed29f7af7e4bfc6
Laxman Desai <lolopaw24@gmail.com>
2021-11-23 23:51:56 +0530

    👌 IMPROVE: Add stats on home page

--- src/components/Home/HomeContainer.jsx
+++ src/components/Home/HomeContainer.jsx
@@ -1,23 +1,92 @@
+import { Empty } from 'antd'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components/macro'
 
-import HomeItem from 'components/Home/HomeItem'
-import { Aside, PageHeading, PageTitle } from 'components/shared'
+import { API } from 'api'
+import {
+  Aside,
+  LoaderAnimation,
+  PageHeading,
+  PageSubtitle,
+  PageTitle,
+} from 'components/shared'
+import { AsideHeader } from 'components/shared/Aside'
+import { StyledCard } from 'components/shared/Card'
+import { toastError } from 'components/toast'
+import { coursePageUrl } from 'helpers/format'
+
+const HomeItem = ({ course, hash }) => {
+  const { code, title } = course
+
+  return (
+    <Link to={coursePageUrl(code, title, hash)}>
+      <StyledCard hoverable>
+        <StyledCard.Meta title={code} description={title} />
+      </StyledCard>
+    </Link>
+  )
+}
 
 const HomeContainer = () => {
+  const [stats, setStats] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      try {
+        setLoading(true)
+        const response = await API.stats.list()
+        setStats(response)
+      } catch (error) {
+        toastError(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchStats()
+  }, [])
+
   return (
     <>
       <PageHeading>
-        <PageTitle>Feed</PageTitle>
+        <PageTitle>Popular Courses</PageTitle>
       </PageHeading>
 
+      <LoaderAnimation disable={!loading} fixed />
+
       <Container>
-        <HomeItem />
-        <HomeItem />
-        <HomeItem />
+        <StatsContainer>
+          <AsideHeader title="Most favourites" loading={loading} />
+          <Flex>
+            {stats?.courses?.popular?.map((course) => (
+              <HomeItem key={course.code} course={course} />
+            ))}
+          </Flex>
+        </StatsContainer>
+
+        <StatsContainer>
+          <AsideHeader title="Most resource requests" loading={loading} />
+          <Flex>
+            {stats?.courses?.requested?.reviews?.map((course) => (
+              <HomeItem key={course.code} course={course} hash="reviews" />
+            ))}
+          </Flex>
+        </StatsContainer>
+
+        <StatsContainer>
+          <AsideHeader title="Most review requests" loading={loading} />
+          <Flex>
+            {stats?.courses?.requested?.resources?.map((course) => (
+              <HomeItem key={course.code} course={course} hash="resources" />
+            ))}
+          </Flex>
+        </StatsContainer>
       </Container>
 
-      <Aside title="Popular">
-        <h1>Hello</h1>
+      <Aside title="Feed">
+        <Empty description={<PageSubtitle>Coming soon!</PageSubtitle>} />
       </Aside>
     </>
   )
@@ -30,4 +99,20 @@ const Container = styled.div`
   flex-direction: column;
   gap: 1rem;
   width: 100%;
+`
+
+const StatsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 1rem;
+  background: ${({ theme }) => theme.secondary};
+  border-radius: 0.5rem;
+`
+
+const Flex = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  gap: 0.5rem;
+  overflow-x: scroll;
 `
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { Helmet } from 'react-helmet-async'
 
-import HomeContainer from 'components/Home/HomeContainer'
+import { HomeContainer } from 'components/Home'
 import { PageContainer } from 'components/shared'
 
 const Home = () => {
   return (
     <PageContainer>
       <Helmet>
-        <title>Courses - ResoBin</title>
+        <title>ResoBin</title>
         <meta name="description" content="Courses availabe at IIT Bombay" />
       </Helmet>
 
