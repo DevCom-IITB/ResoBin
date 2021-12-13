6a1c204c5c3a230ea53bd136987f83b9e7783b80
Kaustav Prasad <kaustav@Kaustavs-MacBook-Air.local>
2021-12-13 18:33:17 +0530

    Added User Profile panel

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,56 @@
+import { UserOutlined } from '@ant-design/icons'
+import { Descriptions } from 'antd'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components/macro'
+
+import { UserAvatar } from 'components/shared'
+import { selectUserProfile } from 'store/userSlice'
+
+const Profile = () => {
+    const profile = useSelector(selectUserProfile)
+    console.log(profile)
+    return (
+        <>
+            <AvatarContainer>
+                <UserAvatar size={72} src={profile?.profilePicture}/>
+            </AvatarContainer>
+            
+            <StyledDescriptions 
+            // title="User Info"
+            column={1}
+            >
+                <Descriptions.Item label="Name">{profile.name}</Descriptions.Item>
+                <Descriptions.Item label="Department">{profile.department}</Descriptions.Item>
+                <Descriptions.Item label="LDAP ID">{profile.ldapId}</Descriptions.Item>
+                <Descriptions.Item label="Email">{profile.email}</Descriptions.Item>
+            </StyledDescriptions>
+        </>
+    )
+}
+
+export default Profile
+
+
+const StyledDescriptions = styled(Descriptions)`
+    padding-left: 0.5rem;
+
+    .ant-descriptions-title{
+        color: ${({ theme }) => theme.textColor};
+    }  
+    .ant-descriptions-item{
+        padding-bottom:0.75rem;
+    }
+    .ant-descriptions-item-label{
+        color: ${({ theme }) => theme.primary};
+    }
+    .ant-descriptions-item-content{
+        color: ${({ theme }) => theme.textColor};
+    }
+`
+
+const AvatarContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-bottom: 1rem;
+`
+
