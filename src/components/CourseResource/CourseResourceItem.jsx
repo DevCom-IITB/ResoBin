bc7cbd92b65340e2d7f21ee53d6f714b7e55ba12
Laxman Desai <lolopaw24@gmail.com>
2022-01-19 08:22:34 +0530

    👌 IMPROVE: Add thumbnail and update field changes

--- src/components/CourseResource/CourseResourceItem.jsx
+++ src/components/CourseResource/CourseResourceItem.jsx
@@ -3,19 +3,14 @@ import {
   InformationCircle,
   PencilAlt,
 } from '@styled-icons/heroicons-outline'
+import { Popover } from 'antd'
 import { rgba } from 'polished'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components/macro'
 
 import placeholderImg from 'assets/images/ResourcePlaceholder.jpg'
-import {
-  ButtonIcon,
-  Popover,
-  Timestamp,
-  toast,
-  UserAvatar,
-} from 'components/shared'
+import { ButtonIcon, Timestamp, toast, UserAvatar } from 'components/shared'
 import { API } from 'config/api'
 import { selectUserProfile } from 'store/userSlice'
 import { limitLines } from 'styles/mixins'
@@ -24,7 +19,7 @@ import CourseResourceItemEditModal from './CourseResourceItemEditModal'
 
 const CourseResourceItem = ({ content: initialContent }) => {
   const { id } = useSelector(selectUserProfile)
-  const isOwner = id === initialContent.userProfile.id
+  const isOwner = id === initialContent.uploadedBy.id
 
   const [editModalVisible, setEditModalVisible] = useState(false)
   const [content, setContent] = useState(initialContent)
@@ -46,7 +41,11 @@ const CourseResourceItem = ({ content: initialContent }) => {
   return (
     <>
       <GridItem>
-        <img src={content.image || placeholderImg} alt={content.title} />
+        <img
+          src={content.thumbnail || placeholderImg}
+          alt={content.title}
+          style={{ width: '100%', height: '100%' }}
+        />
 
         <ItemInfo>
           <ResourceTitle>{content.title}</ResourceTitle>
@@ -76,12 +75,17 @@ const CourseResourceItem = ({ content: initialContent }) => {
                 <PopoverContent>
                   <UserAvatar
                     size="2rem"
-                    src={content?.userProfile.profilePicture}
+                    src={content?.uploadedBy.profilePicture}
                     alt="Profile picture"
                   />
 
                   <PopoverHeading>
-                    <h2>{content?.userProfile.name}</h2>
+                    <h3>
+                      Uploaded by: <b>{content?.uploadedBy.name}</b>
+                    </h3>
+                    <h3>
+                      Author: <b>{content?.author || 'Not available'}</b>
+                    </h3>
                     <span>
                       Uploaded <Timestamp time={content?.timestamp} />
                     </span>
@@ -90,7 +94,6 @@ const CourseResourceItem = ({ content: initialContent }) => {
               }
               title="Information"
               trigger="click"
-              getPopupContainer={(triggerNode) => triggerNode}
             >
               <ButtonIcon
                 size="default"
@@ -183,10 +186,14 @@ const PopoverContent = styled.div`
 `
 
 const PopoverHeading = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
   color: ${({ theme }) => theme.textColorInactive};
 
-  h2 {
+  h3 {
     color: ${({ theme }) => theme.textColor};
     font-size: 0.75rem;
+    font-weight: 400;
   }
 `
