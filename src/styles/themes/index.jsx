48c444de3299cd118e187d44a191f0f2d0b41666
Kaustav Prasad <kaustavprasad@gmail.com>
2022-02-14 05:48:07 +0530

    added valentine's day theme

--- src/styles/themes/index.jsx
+++ src/styles/themes/index.jsx
@@ -1,9 +1,11 @@
 import dark from './dark'
 import light from './light'
+import valentines from './valentines'
 
 const theme = {
   dark,
   light,
+  valentines,
 }
 
 export default theme
---
+++ src/styles/themes/valentines.jsx
@@ -0,0 +1,27 @@
+import globalConfig from './common'
+
+const ValentinesTheme = {
+  primary: '#e3a4c9',
+
+  secondary: '#56192f',
+  secondaryHover: '#601131',
+
+  darksecondary: '#2f1015',
+  darksecondaryHover: '#2c0f12',
+
+  logo: '#c05387',
+  logoHover: '#c15388',
+
+  textColor: '#FFF',
+  textColorInactive: '#aaa5a5',
+
+  placeholder: '#807da0',
+  header: '#FFF',
+  headerNumber: '#585280',
+  activeMenu: '#FFF',
+  courseBG: '#B2BFE1',
+  dividerColor: 'rgba(255, 255, 255, 0.1)',
+  ...globalConfig,
+}
+
+export default ValentinesTheme
