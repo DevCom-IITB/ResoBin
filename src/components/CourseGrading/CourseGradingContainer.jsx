a8c645bffad6b039ff43dfec3d9a7ca44140b563
Surhud Naik <naiksurhud@gmail.com>
2025-06-11 05:02:10 +0530

    dummy bar graph made with dummyData

--- src/components/CourseGrading/CourseGradingContainer.jsx
+++ src/components/CourseGrading/CourseGradingContainer.jsx
@@ -1,24 +1,65 @@
-import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import styled from 'styled-components/macro'
-
 import {
-  ButtonSquare,
-  LoaderAnimation,
-  toast,
-  Tabs,
-  NotFoundSearch,
-} from 'components/shared'
-import { API } from 'config/api'
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+import styled from 'styled-components/macro'
+import dummyData from './ScrapedData'
+
+const data = dummyData
 
 const CourseGradingContainer = () => {
   return (
-    <div className="course-grading-container">
-      <h1>Course Grading</h1>
-      <p>This is the course grading container.</p>
-      {/* Additional components and logic will go here */}
-    </div>
+    <>
+      <Header>
+        <h1>Course Grading</h1>
+      </Header>
+      <Container>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart
+            data={data}
+            margin={{ top: 5, right: 30, left: 50, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="grade" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar
+              dataKey="value"
+              fill="#8884d8"
+              isAnimationActive={true}
+              animationDuration={1500}
+              animationEasing="ease-in-out"
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </Container>
+    </>
   )
 }
 
 export default CourseGradingContainer
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  margin: 1rem 0;
+`
+
+const Container = styled.div`
+  height: 300px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  padding: 1.5rem 1rem;
+  color: ${({ theme }) => theme.textColor};
+  background: ${({ theme }) => theme.secondary};
+  border-radius: ${({ theme }) => theme.borderRadius};
+`
---
+++ src/components/CourseGrading/ScrapedData.js
@@ -0,0 +1,13 @@
+const dummyData = [
+  { grade: 'AP', value: 5 },
+  { grade: 'AA', value: 3 },
+  { grade: 'AB', value: 7 },
+  { grade: 'BB', value: 12 },
+  { grade: 'BC', value: 18 },
+  { grade: 'CD', value: 20 },
+  { grade: 'CC', value: 20 },
+  { grade: 'DD', value: 10 },
+  { grade: 'FR', value: 5 },
+]
+
+export default dummyData
