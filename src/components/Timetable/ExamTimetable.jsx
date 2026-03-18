8fcf888c649bc2ece2ee49e6c90abd0c3dab51ca
ndstab <sajjadgoat2306@gmail.com>
2026-03-18 18:20:59 +0530

    changed midsem to endsem

--- src/components/Timetable/ExamTimetable.jsx
+++ src/components/Timetable/ExamTimetable.jsx
@@ -705,7 +705,7 @@ const PopupExample = () => {
   return (
     <div className="popup">
       <ExamButton type="button" onClick={togglePopup}>
-        Mid Sem
+        End Sem
       </ExamButton>
 
       {isOpen && (
