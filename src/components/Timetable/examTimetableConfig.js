43ea9c9ac07df8461bbbf4c1fc3369964fac6499
ayush <workside@gamil.com>
2026-03-28 10:36:23 +0530

    Fixed Download

---
+++ src/components/Timetable/examTimetableConfig.js
@@ -0,0 +1,29 @@
+export const EXAM_DATE_RANGE = {
+  start: '20/04/26',
+  end: '01/05/26',
+}
+
+export const EXAM_LABEL = 'End-semester Examinations'
+export const EXAM_CALENDAR_NAME = 'End Semester Exams'
+export const EXAM_EVENT_LABEL = 'End Semester Exam'
+
+export const EXAM_TIME_SLOTS = [
+  {
+    label: '08:00 AM - 11:00 AM',
+    slot: 1,
+    startTime: '08:00',
+    endTime: '11:00',
+  },
+  {
+    label: '01:00 PM - 04:00 PM',
+    slot: 2,
+    startTime: '13:00',
+    endTime: '16:00',
+  },
+  {
+    label: '05:30 PM - 08:30 PM',
+    slot: 3,
+    startTime: '17:30',
+    endTime: '20:30',
+  },
+]
